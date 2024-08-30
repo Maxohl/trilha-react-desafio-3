8c91f207972aaafe1dd5e9eab37b84ad3462bd10
Maxohl <maxohl12@gmail.com>
2024-08-30 10:01:46 -0300

    Added Register and fixed server

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -9,6 +9,8 @@ export const ButtonContainer = styled.button`
     padding: 2px 12px;
     min-width: 120px;
     width: 100%;
+    margin-right: 10px;
+    cursor: pointer;
     
     ${({variant}) => variant !== "primary" && css`
         min-width: 167px;
